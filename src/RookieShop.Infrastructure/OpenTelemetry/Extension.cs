062ef8727ed2d5372590e8b7158d5a1547e1ef8b
Nguyen Xuan Nhan <nguyenxuannhan407@gmail.com>
2024-05-11 16:00:55 +0700

    feat(o11y): add observability

---
+++ src/RookieShop.Infrastructure/OpenTelemetry/Extension.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Npgsql;
+using OpenTelemetry.Logs;
+using OpenTelemetry.Metrics;
+using OpenTelemetry.Trace;
+
+namespace RookieShop.Infrastructure.OpenTelemetry;
+
+public static class Extension
+{
+    public static IHostApplicationBuilder AddOpenTelemetry(this IHostApplicationBuilder builder)
+    {
+        builder.Logging.AddOpenTelemetry(logging =>
+        {
+            logging.IncludeFormattedMessage = true;
+            logging.IncludeScopes = true;
+        });
+
+        builder.Services.AddOpenTelemetry()
+            .WithTracing(tracing =>
+            {
+                if (builder.Environment.IsDevelopment())
+                {
+                    tracing.SetSampler(new AlwaysOnSampler());
+                }
+
+                tracing
+                    .AddAspNetCoreInstrumentation()
+                    .AddHttpClientInstrumentation()
+                    .AddEntityFrameworkCoreInstrumentation(b => b.SetDbStatementForText = true)
+                    .AddRedisInstrumentation()
+                    .AddNpgsql();
+            })
+            .WithMetrics(metrics =>
+                metrics
+                    .AddRuntimeInstrumentation()
+                    .AddMeter("Microsoft.AspNetCore.Hosting", "Microsoft.AspNetCore.Server.Kestrel", "System.Net.Http")
+            );
+
+        builder.AddOpenTelemetryExporters();
+
+        return builder;
+    }
+
+    private static void AddOpenTelemetryExporters(this IHostApplicationBuilder builder)
+    {
+        var useOtlpExporter = !string.IsNullOrWhiteSpace(builder.Configuration["OTEL_EXPORTER_OTLP_ENDPOINT"]);
+
+        if (!useOtlpExporter) return;
+
+        builder.Services.Configure<OpenTelemetryLoggerOptions>(logging => logging.AddOtlpExporter());
+        builder.Services.ConfigureOpenTelemetryMeterProvider(metrics => metrics.AddOtlpExporter());
+        builder.Services.ConfigureOpenTelemetryTracerProvider(tracing => tracing.AddOtlpExporter());
+    }
+}
