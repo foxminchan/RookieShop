e34978638eec199ba7875782d9aa62167a2c7cce
Nguyen Xuan Nhan <nguyenxuannhan407@gmail.com>
2024-05-16 23:55:51 +0700

    style: add tailwind to storefront

---
+++ ui/storefront/tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  purge: {
+    enabled: true,
+    content: ["./Pages/**/*.cshtml", "./Views/**/*.cshtml"],
+  },
+  content: [],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
