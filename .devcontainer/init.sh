dcb9b755940b5887d379c8e52d7adf388946e88b
Nguyen Xuan Nhan <nguyenxuannhan407@gmail.com>
2024-06-10 23:15:57 +0700

    ci: update devcontainer

--- .devcontainer/init.sh
+++ .devcontainer/init.sh
@@ -1,4 +1,22 @@
+#!/bin/sh
+
+## Restore the project
 dotnet restore ./RookieShop.sln
+
+## Restore the dotnet tools
 dotnet tool restore
+
+## Restore the npm packages
+npm install --force
+
+## Install Aspire workload
 dotnet workload install aspire
-bun install
+
+## Create a k3d cluster
+while (! kubectl cluster-info ); do
+  echo "Waiting for Docker to launch..."
+  k3d cluster delete
+  k3d registry create foxminchan.localhost --port 8900
+  k3d cluster create -p '8081:80@loadbalancer' --k3s-arg '--disable=traefik@server:0' --registry-use k3d-foxminchan.localhost:8900
+  sleep 1
+done
